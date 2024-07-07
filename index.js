ca8c3110c427f019ffb11c25ae2e25363b7d0955
jojo <georgebadia2019@gmail.com>
2024-07-07 16:48:44 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+const totalBatteries = batteryBatches.reduce((allBatteriesCombined, batch) => allBatteriesCombined + batch, 0);
+
+// the output sum of all the assembled batteries equal to 31
+console.log(batteryBatches);
